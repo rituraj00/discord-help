f49e0f8500496099476ee333b6057f7e0daa237f
Rituraj <77140528+rituraj00@users.noreply.github.com>
2024-04-07 21:15:54 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,15 @@ import cors from 'cors';
 import express from 'express';
 import adminRoutes from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
+const fileUpload = require('express-fileupload');
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use('/public/temp',express.static("public/temp"))
+app.use('/public/temp',express.static("public/temp"));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp' }));
 app.use(cors(
     {
         origin : ["https://deploy-mern-1whq.vercel.app"],
