17bd07a5b826d0ae77de8352785445611e8f3f70
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-04-07 19:50:20 +0530

    'backendfile'

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 app.use('/public/temp',express.static("public/temp"))
-app.use(cors())
+app.use(cors(
+    {
+        origin : ["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credential : true
+    }
+))
 app.use('/user',userRouter)
 app.use('/admin',adminRoutes);
 export { app };
